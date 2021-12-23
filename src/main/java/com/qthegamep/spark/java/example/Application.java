4718a43baaa25e329f262f21220d78405bd9091c
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 13:32:17 +0200

    Added log level configuration to Application class

--- src/main/java/com/qthegamep/spark/java/example/Application.java
+++ src/main/java/com/qthegamep/spark/java/example/Application.java
@@ -1,6 +1,7 @@
 package com.qthegamep.spark.java.example;
 
 import com.qthegamep.spark.java.example.config.ApplicationConfig;
+import com.qthegamep.spark.java.example.config.LogConfig;
 import com.qthegamep.spark.java.example.exception.ApplicationConfigInitializationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,8 @@ public class Application {
     public static void main(String[] args) throws ApplicationConfigInitializationException {
         ApplicationConfig applicationConfig = new ApplicationConfig();
         applicationConfig.init();
+        LogConfig logConfig = new LogConfig();
+        logConfig.configureLogLevels();
         // TODO
         LOG.info("Hello world!");
     }
