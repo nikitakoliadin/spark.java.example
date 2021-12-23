3e10684ad9b9406986f41adf91db3ba7e1d595d5
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 17:45:37 +0200

    Added thread pool settings to Application class

--- src/main/java/com/qthegamep/spark/java/example/Application.java
+++ src/main/java/com/qthegamep/spark/java/example/Application.java
@@ -19,6 +19,10 @@ public class Application {
         logConfig.configureLogLevels();
         int port = Integer.parseInt(System.getProperty("application.port", "8080"));
         port(port);
+        int maxThreads = Integer.parseInt(System.getProperty("application.server.max.threads"));
+        int minThreads = Integer.parseInt(System.getProperty("application.server.min.threads"));
+        int idleTimeout = Integer.parseInt(System.getProperty("application.server.idle.timeout"));
+        threadPool(maxThreads, minThreads, idleTimeout);
         init();
         // TODO
         LOG.info("Hello world!");
