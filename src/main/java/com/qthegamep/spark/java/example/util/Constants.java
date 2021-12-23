a763d02cc66dbce4f1a2f5f7aef61196977ea502
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 12:58:38 +0200

    Created Constants class

---
+++ src/main/java/com/qthegamep/spark/java/example/util/Constants.java
@@ -0,0 +1,8 @@
+package com.qthegamep.spark.java.example.util;
+
+public class Constants {
+
+    public static final String SERVER_IP_PROPERTY = "serverIp";
+    public static final String DEFAULT_CONFIG_PROPERTIES_PATH = "/config.properties";
+    public static final String DOCKER_IMAGE_NAME_PROPERTY = "docker.image.name";
+}
