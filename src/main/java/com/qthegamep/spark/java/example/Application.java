6a86b12ae9f6a6d55710ddb60dc9dbf40ded835e
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 11:50:39 +0200

    Added logger to Application class

--- src/main/java/com/qthegamep/spark/java/example/Application.java
+++ src/main/java/com/qthegamep/spark/java/example/Application.java
@@ -1,9 +1,14 @@
 package com.qthegamep.spark.java.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         // TODO
-        System.out.println("Hello world!");
+        LOG.info("Hello world!");
     }
 }
