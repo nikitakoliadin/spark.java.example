88537c91e76d7ba1a41670a107ce3d3cefeb7313
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 11:20:29 +0200

    Created Application class

---
+++ src/main/java/com/qthegamep/spark/java/example/Application.java
@@ -0,0 +1,9 @@
+package com.qthegamep.spark.java.example;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO
+        System.out.println("Hello world!");
+    }
+}
