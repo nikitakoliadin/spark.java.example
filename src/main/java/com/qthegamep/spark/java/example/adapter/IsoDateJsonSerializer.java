00b24c124df5ed53d0e50bd56d8056cf30654d78
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 19:23:29 +0200

    Created IsoDateJsonSerializer class

---
+++ src/main/java/com/qthegamep/spark/java/example/adapter/IsoDateJsonSerializer.java
@@ -0,0 +1,24 @@
+package com.qthegamep.spark.java.example.adapter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.qthegamep.spark.java.example.util.Constants;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+class IsoDateJsonSerializer extends JsonSerializer<Date> {
+
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.MONGO_UTC_DATE_FORMAT);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone(Constants.GMT_TIMEZONE));
+        String isoDate = simpleDateFormat.format(date);
+        jsonGenerator.writeStartObject(Constants.JSON_DATE_FIELD_NAME);
+        jsonGenerator.writeObjectField(Constants.JSON_DATE_FIELD_NAME, isoDate);
+        jsonGenerator.writeEndObject();
+    }
+}
