9c63670c79f3f0237d397ac9d3b8ea4aa011d053
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-23 13:27:57 +0200

    Added logger constants to Constants class

--- src/main/java/com/qthegamep/spark/java/example/util/Constants.java
+++ src/main/java/com/qthegamep/spark/java/example/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String SERVER_IP_PROPERTY = "serverIp";
     public static final String DEFAULT_CONFIG_PROPERTIES_PATH = "/config.properties";
     public static final String DOCKER_IMAGE_NAME_PROPERTY = "docker.image.name";
+    public static final String LOGGER_REPLACE_PATTERN = "\\{}";
 }
