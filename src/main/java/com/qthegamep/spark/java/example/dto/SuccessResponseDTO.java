5165c5849a640a0396940d6b856c8fd548e5b0fb
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-24 13:21:39 +0200

    Added requestId field to SuccessResponseDTO class

--- src/main/java/com/qthegamep/spark/java/example/dto/SuccessResponseDTO.java
+++ src/main/java/com/qthegamep/spark/java/example/dto/SuccessResponseDTO.java
@@ -12,6 +12,11 @@ public class SuccessResponseDTO {
             required = true)
     private Date now;
 
+    @JsonProperty(
+            value = "requestId",
+            required = true)
+    private String requestId;
+
     public Date getNow() {
         return now;
     }
@@ -20,23 +25,33 @@ public class SuccessResponseDTO {
         this.now = now;
     }
 
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SuccessResponseDTO that = (SuccessResponseDTO) o;
-        return Objects.equals(now, that.now);
+        return Objects.equals(now, that.now)
+                && Objects.equals(requestId, that.requestId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(now);
+        return Objects.hash(now, requestId);
     }
 
     @Override
     public String toString() {
         return "SuccessResponseDTO{" +
                 "now=" + now +
+                ", requestId='" + requestId + '\'' +
                 '}';
     }
 }
