e58876365be083aea1e96b856bb17c91343490eb
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-24 10:30:45 +0200

    Created GenerationServiceImpl class

---
+++ src/main/java/com/qthegamep/spark/java/example/service/GenerationServiceImpl.java
@@ -0,0 +1,27 @@
+package com.qthegamep.spark.java.example.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.SecureRandom;
+
+public class GenerationServiceImpl implements GenerationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GenerationServiceImpl.class);
+
+    @Override
+    public String generateUniqueId(Long length) {
+        StringBuilder uniqueId = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; ++i) {
+            boolean upperCase = secureRandom.nextBoolean();
+            char symbol = (char) (secureRandom.nextInt(26) + 97);
+            if (upperCase) {
+                symbol = Character.toUpperCase(symbol);
+            }
+            uniqueId.append(symbol);
+        }
+        LOG.debug("Generated unique ID: {}", uniqueId);
+        return uniqueId.toString();
+    }
+}
