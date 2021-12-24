889e4047960d8defcc12f80aafd98ae6498fc271
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-24 10:59:17 +0200

    Added requestId constants to Constants class

--- src/main/java/com/qthegamep/spark/java/example/util/Constants.java
+++ src/main/java/com/qthegamep/spark/java/example/util/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final String MONGO_UTC_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String GMT_TIMEZONE = "GMT";
     public static final String JSON_DATE_FIELD_NAME = "$date";
+    public static final String REQUEST_ID_HEADER = "requestId";
+    public static final String X_REQUEST_ID_HEADER = "x-request-id";
 }
