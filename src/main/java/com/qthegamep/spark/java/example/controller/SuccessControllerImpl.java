9783db72491a5b702947995a57fa635192439ec2
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-24 09:59:13 +0200

    Added model mapping to SuccessControllerImpl class

--- src/main/java/com/qthegamep/spark/java/example/controller/SuccessControllerImpl.java
+++ src/main/java/com/qthegamep/spark/java/example/controller/SuccessControllerImpl.java
@@ -1,6 +1,7 @@
 package com.qthegamep.spark.java.example.controller;
 
 import com.qthegamep.spark.java.example.dto.SuccessResponseDTO;
+import com.qthegamep.spark.java.example.service.ConverterService;
 import com.qthegamep.spark.java.example.util.Paths;
 
 import java.util.Date;
@@ -9,12 +10,18 @@ import static spark.Spark.*;
 
 public class SuccessControllerImpl implements SuccessController {
 
+    private ConverterService converterService;
+
+    public SuccessControllerImpl(ConverterService converterService) {
+        this.converterService = converterService;
+    }
+
     @Override
     public void initSuccessController() {
         get(Paths.SUCCESS_PATH, (request, response) -> {
             SuccessResponseDTO successResponseDTO = new SuccessResponseDTO();
             successResponseDTO.setNow(new Date());
             return successResponseDTO;
-        });
+        }, converterService::toJson);
     }
 }
