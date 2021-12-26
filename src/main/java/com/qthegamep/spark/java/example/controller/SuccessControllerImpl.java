88ab12c23f73f7c29697be34b32e552615a7fa58
Nikita Koliadin <qThegamEp@gmail.com>
2021-12-26 21:43:24 +0200

    Added response type in SuccessControllerImpl class

--- src/main/java/com/qthegamep/spark/java/example/controller/SuccessControllerImpl.java
+++ src/main/java/com/qthegamep/spark/java/example/controller/SuccessControllerImpl.java
@@ -1,9 +1,11 @@
 package com.qthegamep.spark.java.example.controller;
 
+import com.qthegamep.spark.java.example.dto.SuccessResponseDTO;
 import com.qthegamep.spark.java.example.service.ConverterService;
 import com.qthegamep.spark.java.example.service.SuccessService;
 import com.qthegamep.spark.java.example.util.Constants;
 import com.qthegamep.spark.java.example.util.Paths;
+import org.eclipse.jetty.http.MimeTypes;
 
 import static spark.Spark.*;
 
@@ -22,7 +24,9 @@ public class SuccessControllerImpl implements SuccessController {
     public void initSuccessController() {
         get(Paths.SUCCESS_PATH, (request, response) -> {
             String requestId = request.attribute(Constants.REQUEST_ID_HEADER);
-            return successService.success(requestId);
+            SuccessResponseDTO successResponseDTO = successService.success(requestId);
+            response.type(MimeTypes.Type.APPLICATION_JSON_UTF_8.asString());
+            return successResponseDTO;
         }, converterService::toJson);
     }
 }
